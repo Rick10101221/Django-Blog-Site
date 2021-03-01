bf005a0aba9dbae8cc36f6157e6e5b11de877a98
Rick10101221 <rikhilna@ucsd.edu>
2021-02-28 18:55:28 -0800

    finished part 3

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+  {
+    'author': 'Rickesh Khilnani',
+    'title': 'Im a dog',
+    'content': 'First',
+    "date_posted": 'August 27, 2020'
+  },
+  {
+    'author': 'Corey in the House',
+    'title': 'Im Corey',
+    'content': 'Second omegalul',
+    "date_posted": 'August 28, 2020'
+  }
+]
 
 def home(request):
-  return HttpResponse('<h1>Blog Home</h1>')
+  context = {
+    'posts': posts
+  }
+  return render(request, 'blog/home.html', context)
 
 
 def about(request):
-  return HttpResponse('<h1>Blog About</h1>')
+  return render(request, 'blog/about.html', {'title': 'About'})
